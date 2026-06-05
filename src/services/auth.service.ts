ded70f41d4ea1a609442e78eb97a1b13253f8f4e
Eswarpulaparthi <eswarnani666@gmail.com>
2026-06-05 17:00:52 +0530

    Set port number and secure to false

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -22,10 +22,13 @@ export class AuthService {
 
   constructor() {
     this.transporter = nodemailer.createTransport({
-      service: "Gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      requireTLS: true,
       auth: {
-        user: process.env.EMAIL_USER!,
-        pass: process.env.EMAIL_PASS!,
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
   }
